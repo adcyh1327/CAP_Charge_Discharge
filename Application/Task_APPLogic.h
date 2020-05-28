6bfebae97048ed60d08fbea0b935356cc1ecc471
Trumming <xcm0215@163.com>
2020-05-29 06:00:03 +0800

    add Application files;

--- Application/Task_APPLogic.h
+++ Application/Task_APPLogic.h
@@ -6,5 +6,11 @@
 
 void APP_Logic_Init(void);
 void APP_Logic_MainFunction(void *p_arg);
+uint8_t app_device_warning_indicator_out(void);
+uint8_t app_device_warning_indicator_out(void);
+uint8_t app_test_cap_normal_signal_out(void);
+uint8_t app_device_abnormal_signal_out(void);
+uint8_t app_get_device_enable_signal(void);
+uint8_t app_get_device_start_signal(void);
 
 #endif
